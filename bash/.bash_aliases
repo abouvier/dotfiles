ae1aef0c64cba64855cd52a82b18b5462c356183
abouvier <abouvier@student.42.fr>
2015-08-24 19:15:48 +0200

    init

---
+++ bash/.bash_aliases
@@ -0,0 +1,15 @@
+alias ls='ls -hF --color=auto'
+alias ll='ls -l'
+alias la='ll -a'
+
+alias df='df -h'
+
+alias e='emacs'
+
+alias installed_packages='comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n "s/^Package: //p" | sort -u)'
+
+fh() {
+	find -L /usr/include/ -type f -name "*.h" -exec grep --color=auto -Hn "$*" {} +
+}
+
+alias ldap42='ldapsearch -H ldaps://ldap.42.fr -b dc=42,dc=fr -D uid=abouvier,ou=august,ou=2013,ou=paris,ou=people,dc=42,dc=fr -y ~/.ldap/passwd'
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,18 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(global-linum-mode t)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode t)
+(global-set-key (kbd "DEL") 'backward-delete-char)
+(setq-default c-backspace-function 'backward-delete-char)
+(setq-default c-basic-offset 4)
+(setq-default c-default-style "linux")
+
+(autoload 'apache-mode "apache-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
+(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
+(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
+(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
