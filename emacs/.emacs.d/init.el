a0d3001dc99ac87a12f79efbc53ddb3f5d7973b9
abouvier <abouvier@student.42.fr>
2016-01-19 01:46:52 +0100

    split init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,25 +1,26 @@
-:(require 'package) ;; You might already have this line
-(add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+;;; init.el --- Init File
+;;; Commentary:
+;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/elisp/")
+(load "~/.emacs.d/melpa")
 
 (setq auto-save-default nil)
 (setq make-backup-files nil)
-(global-linum-mode t)
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode t)
 (setq-default c-basic-offset 4)
 (setq-default c-default-style "linux")
 (setq vc-follow-symlinks nil)
+(setq column-number-mode t)
+(global-linum-mode t)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (autoload 'apache-mode "apache-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
 (add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
+(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
 (add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;; init.el ends here
