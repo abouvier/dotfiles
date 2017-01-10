587e603da52ddb9d4d7a6c85c61dedc4f4feb068
abouvier <abouvier@student.42.fr>
2017-01-10 06:41:37 +0100

    remove apache mdoe

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,10 +14,3 @@
 
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 (add-hook 'after-init-hook #'global-flycheck-mode)
-
-(autoload 'apache-mode "apache-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
-(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
-(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
-(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
-(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
