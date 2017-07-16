7fe29119d9822e91c9c523119c577481adaaf25d
Alexandre Bouvier <contact@amb.tf>
2017-07-17 00:30:52 +0200

    reduce timeout

--- aarchup/.config/autostart-scripts/aarchup.sh
+++ aarchup/.config/autostart-scripts/aarchup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 killall -9 --quiet aarchup
-aarchup --loop-time=60 --aur --icon=/usr/share/aarchup/archlogo.svg &
+aarchup --loop-time=60 --aur --icon=/usr/share/aarchup/archlogo.svg --timeout=5 &
