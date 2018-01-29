a21ab5e7d14bcf3417f51fb232092008cc12aa4c
Alexandre Bouvier <contact@amb.tf>
2018-01-29 17:29:50 +0100

    nvidia: move some vars from xprofile to profile

---
+++ nvidia/.config/profile.d/nvidia.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export __GL_SHADER_DISK_CACHE_PATH=${XDG_CACHE_HOME:-$HOME/.cache}/nvidia
+export CUDA_CACHE_PATH=${XDG_CACHE_HOME:-$HOME/.cache}/nvidia
--- nvidia/.config/xprofile.d/nvidia.sh
+++ nvidia/.config/xprofile.d/nvidia.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export __GL_SHADER_DISK_CACHE_PATH=${XDG_CACHE_HOME:-$HOME/.cache}/nvidia
 export LIBVA_DRIVER_NAME=vdpau
 export VDPAU_DRIVER=nvidia
 
