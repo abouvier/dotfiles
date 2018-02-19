610d59cd0b36650ba332c45631d3bd5b0ac91d4d
Alexandre Bouvier <contact@amb.tf>
2018-02-19 21:52:06 +0100

    bash-completion: enable completion for aliases

---
+++ bash-completion/.config/bash.d/bash-completion.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+complete -F _complete_alias q
+complete -F _complete_alias qi
+complete -F _complete_alias ql
+complete -F _complete_alias si
+complete -F _complete_alias t
---
+++ bash-completion/.config/profile.d/bash-completion.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+export BASH_COMPLETION_USER_FILE=~/.local/lib/complete-alias/bash_completion
