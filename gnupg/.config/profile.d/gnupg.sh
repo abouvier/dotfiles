cfa25e50ef96f393b6af4b69ad92aa045e0abaa4
Alexandre Bouvier <contact@amb.tf>
2018-03-17 20:00:43 +0100

    openssh: replace gpg-agent by ssh-agent for ssh keys

--- gnupg/.config/profile.d/gnupg.sh
+++ gnupg/.config/profile.d/gnupg.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 export GNUPGHOME=${XDG_CONFIG_HOME:-$HOME/.config}/gnupg
-# shellcheck disable=SC2155
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-
-gpg-connect-agent /bye
---
+++ openssh/.config/profile.d/openssh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if ! pgrep -u "$USER" ssh-agent >/dev/null ; then
+	ssh-agent > "$XDG_RUNTIME_DIR"/ssh-agent
+fi
+
+eval " $(cat "$XDG_RUNTIME_DIR"/ssh-agent)"
