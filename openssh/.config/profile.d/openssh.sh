e1d5c042310b085d24cd8bb645576622d2ffa935
Alexandre Bouvier <contact@amb.tf>
2018-03-23 09:56:12 +0100

    openssh: silence eval

--- openssh/.config/profile.d/openssh.sh
+++ openssh/.config/profile.d/openssh.sh
@@ -3,4 +3,4 @@ if ! pgrep -u "$USER" ssh-agent >/dev/null ; then
 	ssh-agent > "$XDG_RUNTIME_DIR"/ssh-agent
 fi
 
-eval " $(cat "$XDG_RUNTIME_DIR"/ssh-agent)"
+eval " $(cat "$XDG_RUNTIME_DIR"/ssh-agent)" >/dev/null
