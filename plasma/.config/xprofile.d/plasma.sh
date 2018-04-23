b05d4cfd6533696ed42ceefd8a33b1d9f12980d4
Alexandre Bouvier <contact@amb.tf>
2018-04-23 23:02:19 +0200

    plasma: fix $XDG_DATA_DIRS

--- plasma/.config/xprofile.d/plasma.sh
+++ plasma/.config/xprofile.d/plasma.sh
@@ -2,4 +2,5 @@
 if [ "$XDG_CURRENT_DESKTOP" = KDE ] ; then
 	mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/dbus-1/services
 	ln -sf -T /usr/share/dbus-1/services/org.kde.plasma.Notifications.service "${XDG_DATA_HOME:-$HOME/.local/share}"/dbus-1/services/org.freedesktop.Notifications.service
+	export XDG_DATA_DIRS=/usr/local/share:/usr/share
 fi
