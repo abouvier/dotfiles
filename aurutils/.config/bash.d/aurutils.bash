5601ef82aa6f4b531fca00491efdd1f39075d316
Alexandre Bouvier <contact@amb.tf>
2018-04-28 00:41:23 +0200

    aurutils: update ud alias

--- aurutils/.config/bash.d/aurutils.bash
+++ aurutils/.config/bash.d/aurutils.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-alias ud='aur sync --force --no-ver --no-view --rmdeps -- $(pacman -Slq aur | grep -E -- "-(bzr|git|hg|svn)$")'
+alias ud='aur vercmp-devel | cut -d: -f1 | aur sync --no-ver-shallow -'
 alias pacwoman='pacman --config=<(sed /aur/d /etc/pacman.conf)'
 alias uninstalled_packages='unbuffer pacman -Sl aur | grep -v install'
 
