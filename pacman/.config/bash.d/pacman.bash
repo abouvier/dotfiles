66a26b2c33e1077081feab87bd654dd3b21c3106
Alexandre Bouvier <contact@amb.tf>
2018-04-28 00:36:26 +0200

    pacman: alias optimization

--- pacman/.config/bash.d/pacman.bash
+++ pacman/.config/bash.d/pacman.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-alias installed_packages="comm -23 \
-	<(pacman -Qeq | sort) \
-	<(pacman -Qgq base base-devel plasma | sort)"
+alias installed_packages="pacman -Qeq | sort \
+	| comm -23 - <(pacman -Qgq base base-devel plasma | sort)"
 alias si='pacman -Si'
 alias q='pacman -Qi'
 alias ql='pacman -Ql'
 alias qo='pacman -Qo'
 alias r='sudo pacman -Rcss'
-alias last_installed='expac -t %s %l\\t%n $(installed_packages) | sort | cut -f2'
+alias last_installed="expac --timefmt=%s '%l\\t%n' \$(installed_packages) \
+	| sort | cut -f2"
 alias useless_packages='pacman -Qdtt'
