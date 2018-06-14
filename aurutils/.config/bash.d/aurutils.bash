145e82dec8aac41bd3f5b74c9c7dd5fd9ba2a729
Alexandre Bouvier <contact@amb.tf>
2018-06-14 05:32:45 +0200

    aurutils: remove useless check

--- aurutils/.config/bash.d/aurutils.bash
+++ aurutils/.config/bash.d/aurutils.bash
@@ -4,7 +4,6 @@ alias uninstalled_packages='unbuffer pacman -Sl aur | grep -v install'
 
 u () {
 	sudo pacman -Syu -- "$@"
-	(($? == 130)) && return 130
 	local packages=${XDG_CONFIG_HOME:-$HOME/.config}/aurutils/sync/ignore
 	aur vercmp-devel | cut -d: -f1 | aur sync --no-ver-shallow --upgrades \
 		--ignore="$(paste -sd, "$packages")" --rmdeps -- "$@" -
