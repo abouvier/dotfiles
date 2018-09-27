ccfd8d6ba39b8ea5032dda226c3035f50e169a3f
Alexandre Bouvier <contact@amb.tf>
2018-09-27 19:41:18 +0200

    aurutils: update u alias

--- aurutils/.config/bash.d/aurutils.bash
+++ aurutils/.config/bash.d/aurutils.bash
@@ -3,10 +3,10 @@ alias pacwoman='pacman --config=<(sed /aur/d /etc/pacman.conf)'
 alias uninstalled_packages='unbuffer pacman -Sl aur | grep -v install'
 
 u () {
-	sudo pacman -Syu -- "$@"
+	sudo pacman -Syu -- "$@" || return
 	local packages=${XDG_CONFIG_HOME:-$HOME/.config}/aurutils/sync/ignore
-	aur vercmp-devel | cut -d: -f1 | aur sync --no-ver-shallow --upgrades \
-		--ignore="$(paste -sd, "$packages")" --rmdeps -- "$@" -
+	aur vercmp-devel | cut -d: -f1 | xargs aur sync --nover-shallow --upgrades \
+		--ignore="$(sed '/^#/d' "$packages" | fmt -1 | paste -sd,)" -- "$@"
 }
 
 s () {
