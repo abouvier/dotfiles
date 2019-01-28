10318f5fec58c5bec021c0580aea4ac601d7014e
Alexandre Bouvier <contact@amb.tf>
2019-01-29 00:44:16 +0100

    tdm: update session names

--- dunst/.config/X11/xinit/xinitrc.d/dunst.sh
+++ dunst/.config/X11/xinit/xinitrc.d/dunst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 session=${XDG_CONFIG_HOME:-$HOME/.config}/tdm/default
 
-if ! readlink "$session" | grep -q plasma ; then
+if ! readlink "$session" | grep -q Plasma ; then
 	dbus=${XDG_DATA_HOME:-$HOME/.local/share}/dbus-1/services
 	mkdir -p "$dbus"
 	ln -sf -T /usr/share/dbus-1/services/org.knopwob.dunst.service \
--- qt5ct/.config/X11/xinit/xinitrc.d/qt5ct.sh
+++ qt5ct/.config/X11/xinit/xinitrc.d/qt5ct.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 session=${XDG_CONFIG_HOME:-$HOME/.config}/tdm/default
 
-if ! readlink "$session" | grep -q plasma ; then
+if ! readlink "$session" | grep -q Plasma ; then
 	export QT_QPA_PLATFORMTHEME=qt5ct
 fi
 
