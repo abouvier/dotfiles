3f85c186b1c5472ed8acba17d67c9629feeda293
Alexandre Bouvier <contact@amb.tf>
2020-02-27 04:01:08 +0100

    bash-completion: update alias list

--- bash-completion/.config/bashrc.d/bash-completion.bash
+++ bash-completion/.config/bashrc.d/bash-completion.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
+complete -F _complete_alias l
 complete -F _complete_alias q
-complete -F _complete_alias qi
 complete -F _complete_alias ql
+complete -F _complete_alias r
 complete -F _complete_alias si
 complete -F _complete_alias t
