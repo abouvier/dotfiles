32052bd15c038355af9bd6c87d455a678d46ad7e
Alexandre Bouvier <contact@amb.tf>
2020-05-30 11:04:10 +0200

    pacman: update aliases

--- pacman/.config/bashrc.d/pacman.bash
+++ pacman/.config/bashrc.d/pacman.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
-alias installed_packages="pacman -Qeq | sort \
-	| comm -23 - <(pacman -Qgq base-devel plasma | sort)"
+alias exclude_groups='sort | comm -23 - <(pacman -Qgq base-devel plasma | sort)'
+alias installed_packages='pacman -Qe'
 alias si='pacman -Si'
 alias q='pacman -Qi'
 alias ql='pacman -Ql'
 alias qo='pacman -Qo'
 alias r='sudo pacman -Rcss'
-alias last_installed="expac --timefmt=%s '%l\\t%n' \$(installed_packages) \
-	| sort | cut -f2"
-alias useless_packages='pacman -Qdtt'
+alias last_installed="installed_packages | expac --timefmt=%s '%l\\t%n' - \
+	| sort | cut -f2 | xargs -r pacman -Q"
+alias useless_packages='pacman -Qdttq | exclude_groups | xargs -r pacman -Q'
+alias msi='test -e PKGBUILD && makepkg --printsrcinfo > .SRCINFO'
+alias modified_configs='pacman -Qii | grep ^MODIFIED | cut -f2 | sort'
+alias foreign_packages='pacman -Qm'
