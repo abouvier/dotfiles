66485046d2db7ce6e529fb478878b598a8d75e45
Alexandre Bouvier <contact@amb.tf>
2020-06-22 04:37:59 +0200

    aurutils: add bl alias

--- aurutils/.config/bashrc.d/aurutils.bash
+++ aurutils/.config/bashrc.d/aurutils.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 alias aur_repo='pacman-conf --repo-list | grep -- -aur$'
+alias local_repo='pacman-conf --repo-list | grep -- -local$'
 alias uninstalled_packages='unbuffer pacman -Sl $(pacman-conf --repo-list | egrep "aur|local") | grep -v install'
 alias b='aur sync --chroot --database="$(aur_repo)" --rebuild'
+alias bl='aur build --chroot --database="$(local_repo)" --pacman-conf=$HOME/.local/share/devtools/pacman-aur.conf'
 
 u () {
 	local packages=${XDG_CONFIG_HOME:-$HOME/.config}/aurutils/sync/ignore
