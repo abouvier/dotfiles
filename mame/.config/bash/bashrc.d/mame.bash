cfb3fe5321dccaba9cc0971e26e1e968ef609eb0
Alexandre Bouvier <contact@amb.tf>
2020-06-22 18:37:46 +0200

    mame: improve mameverify function

--- mame/.config/bash/bashrc.d/mame.bash
+++ mame/.config/bash/bashrc.d/mame.bash
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 mameverify () {
-	find "${1:-$HOME/Jeux/Arcade}" -iname '*.zip' -print0 \
-		| xargs -0 -L1 basename --suffix=.zip --zero \
+	find "${1:-$HOME/Jeux/Arcade}" -name '*.zip' -type f -print0 \
+		| xargs -0 basename --suffix=.zip --zero \
 		| xargs -0 mame -verifyroms
 }
