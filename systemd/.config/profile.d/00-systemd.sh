92e549b57928f96a3e8dc1482a4a328845da13be
Alexandre Bouvier <contact@amb.tf>
2020-09-02 18:19:49 +0200

    systemd: fix profile

--- systemd/.config/profile.d/00-systemd.sh
+++ systemd/.config/profile.d/00-systemd.sh
@@ -1,7 +1,10 @@
 # shellcheck shell=sh
 if [ -d /usr/lib/systemd/user-environment-generators ]; then
+	flags=$(set +o)
+	set -o allexport
 	for generator in /usr/lib/systemd/user-environment-generators/*; do
-		[ -x "$generator" ] && export "$("$generator")"
+		[ -x "$generator" ] && eval "$("$generator")"
 	done
 	unset generator
+	eval "$flags"
 fi
