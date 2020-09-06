349a5526570c3ce30ab3ba56e4a16272601783df
Alexandre Bouvier <contact@amb.tf>
2020-09-06 05:47:14 +0200

    systemd: update profile script

--- systemd/.config/profile.d/00-systemd.sh
+++ systemd/.config/profile.d/00-systemd.sh
@@ -1,10 +1,10 @@
 # shellcheck shell=sh
 if [ -d /usr/lib/systemd/user-environment-generators ]; then
-	flags=$(set +o)
+	option=$(set +o | grep allexport)
 	set -o allexport
 	for generator in /usr/lib/systemd/user-environment-generators/*; do
 		[ -x "$generator" ] && eval "$("$generator")"
 	done
 	unset generator
-	eval "$flags"
+	eval "$option"
 fi
