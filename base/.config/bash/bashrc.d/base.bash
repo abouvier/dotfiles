9d68ea40d13e2505741911d5772752523a12ccb3
Alexandre Bouvier <contact@amb.tf>
2021-02-07 08:30:17 +0100

    base: use run-parts

--- base/.config/bash/bashrc.d/base.bash
+++ base/.config/bash/bashrc.d/base.bash
@@ -1,20 +1,8 @@
 # shellcheck shell=bash
 s() {
-	local search
-	if [[ -d ${XDG_DATA_HOME:-$HOME/.local/lib}/base/search ]]; then
-		for search in "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/search/*; do
-			[[ -x $search ]] && "$search" "$@"
-		done
-	fi
+	run-parts "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/search
 }
 
 u() {
-	local update
-	if [[ -d ${XDG_DATA_HOME:-$HOME/.local/lib}/base/update ]]; then
-		for update in "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/update/*; do
-			if [[ -x $update ]]; then
-				"$update" "$@" || break
-			fi
-		done
-	fi
+	run-parts --exit-on-error "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/update
 }
--- systemd/.config/profile.d/00-systemd.sh
+++ systemd/.config/profile.d/00-systemd.sh
@@ -1,11 +1,8 @@
 # shellcheck shell=sh
 if [ -d /usr/lib/systemd/user-environment-generators ]; then
-	option=$(set +o | grep allexport)
+	allexport=$(set +o | grep allexport)
 	set -o allexport
-	for generator in /usr/lib/systemd/user-environment-generators/*; do
-		[ -x "$generator" ] && eval "$("$generator")"
-	done
-	unset generator
-	eval "$option"
-	unset option
+	eval "$(run-parts /usr/lib/systemd/user-environment-generators)"
+	eval "$allexport"
+	unset allexport
 fi
