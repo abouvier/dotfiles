233d73e7e4bc4f75a6013ba1732895c06e8b7890
Alexandre Bouvier <contact@amb.tf>
2021-02-07 08:34:55 +0100

    base: use dir.d suffix

--- base/.config/bash/bashrc.d/base.bash
+++ base/.config/bash/bashrc.d/base.bash
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 s() {
-	run-parts "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/search
+	run-parts "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/search.d
 }
 
 u() {
-	run-parts --exit-on-error "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/update
+	run-parts --exit-on-error "${@/#/--arg=}" -- "${XDG_DATA_HOME:-$HOME/.local/lib}"/base/update.d
 }
