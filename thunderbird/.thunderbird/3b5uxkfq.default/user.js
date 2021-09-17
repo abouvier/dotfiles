e8b708d2ce528a3f74424022ba8bafd4c975504e
Alexandre Bouvier <contact@amb.tf>
2021-09-18 00:20:20 +0200

    thunderbird: add user prefs

---
+++ thunderbird/.thunderbird/3b5uxkfq.default/user.js
@@ -0,0 +1,4 @@
+user_pref("gfx.font_rendering.fontconfig.max_generic_substitutions", 10);
+user_pref("gfx.webrender.force-disabled", true);
+user_pref("layout.frame_rate", 144);
+user_pref("widget.use-xdg-desktop-portal", true);
