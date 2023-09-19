1f9c6e73f09f3ba45173aa52887c57a120298647
Alexandre Bouvier <contact@amb.tf>
2023-09-20 01:10:40 +0200

    emacs: use melpa packages

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2,11 +2,11 @@
 (require 'desktop-entry-mode)
 (require 'flycheck)
 (require 'generic-x)
-(require 'pkg-info)
+(require 'package)
 (require 'xdg)
 
-(defun xdg-state-home ()
-	(xdg--dir-home "XDG_STATE_HOME" "~/.local/state"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
 (setq custom-file (expand-file-name "emacs/custom.el" (xdg-state-home)))
 (if (file-exists-p custom-file)
@@ -28,23 +28,8 @@
 
 (add-to-list 'term-file-aliases '("alacritty" . "xterm"))
 
-(autoload 'apache-mode "apache-mode" nil t)
-(add-to-list 'auto-mode-alist '("/httpd\\.conf\\'" . apache-mode))
-(add-to-list 'auto-mode-alist '("^/etc/httpd/conf/.*\\.conf\\'" . apache-mode))
-(add-to-list 'auto-mode-alist '("\\.htaccess\\'" . apache-mode))
-
-(autoload 'lua-mode "lua-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
-(autoload 'php-mode "php-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-(add-to-list 'interpreter-mode-alist '("php" . php-mode))
-
 (setq-default pkgbuild-source-directory-locations ".:~/.cache/pacman/src")
 (setq-default pkgbuild-update-sums-on-save nil)
-(autoload 'pkgbuild-mode "pkgbuild-mode" nil t)
-(add-to-list 'auto-mode-alist '("/PKGBUILD\\'" . pkgbuild-mode))
 
 (setq-default display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
